bbabdbed8ebb6976bd9f8a8a9b63c23cb0c03a88
sankalp-sangle <sanku.sangle@gmail.com>
2020-03-26 02:43:50 +0800

    Added facility to notify via sound on update.

--- corona.py
+++ corona.py
@@ -1,23 +1,24 @@
+import os
 import time
 
 import requests
-
 from prometheus_client import start_http_server
 from prometheus_client.core import (REGISTRY, CounterMetricFamily,
                                     GaugeMetricFamily)
-
 
 url = "https://api.covid19india.org/data.json"
 
 responseObj = requests.request("GET", url, headers={}, data = {})
 response = responseObj.json()
 
+initialValue = None
 
 class CustomCollector(object):
     def __init__(self):
         pass
 
     def collect(self):
+        global initialValue
         print("Call made")
         
         responseObj = requests.request("GET", url, headers={}, data = {})
@@ -27,6 +28,14 @@ class CustomCollector(object):
         g = GaugeMetricFamily("total_cases_confirmed", 'Total diagnosed cases in India')
         g.add_metric([], response['statewise'][0]['confirmed'])
         yield g
+
+        if initialValue is None:
+            initialValue = response['statewise'][0]['confirmed']
+
+        if initialValue != response['statewise'][0]['confirmed']:
+            os.system("say 'There is an update!'")
+            initialValue = response['statewise'][0]['confirmed']
+
 
         g = GaugeMetricFamily("total_cases_recovered", 'Total recovered cases in India')
         g.add_metric([], response['statewise'][0]['recovered'])
